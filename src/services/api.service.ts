b6008474c52669249a6c8cd3030c63d3e691157f
Gabrielklopes <gabrielklopes21@gmail.com>
2024-08-24 13:27:00 -0300

    fix

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-export const api = axios.create({baseURL: 'http://localhost:4000', withCredentials: true})
+export const api = axios.create({
+  baseURL: 'https://verzel-app-back.vercel.app',
+  withCredentials: true
+});
