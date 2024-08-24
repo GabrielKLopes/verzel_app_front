796194b11bdc8fbe29d8b61d46cb08213568ca04
Gabrielklopes <gabrielklopes21@gmail.com>
2024-08-24 17:05:09 -0300

    fix

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.withCredentials = true;
 export const api = axios.create({
   baseURL: 'https://verzel-app-back.vercel.app',
-  withCredentials: true
+  
 });
--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -10,10 +10,6 @@ export const RegisterUser = {
     register: async (registerUser: RegisterUserInteface) =>{
         try{
             const response = await api.post('/session/user/register', registerUser);
-            if(response.data && response.data.token){
-                Cookies.set('token', response.data.token, {expires : 7});
-            }
-
             return response.data;
         }catch(error){
             throw new Error('Register failed');
